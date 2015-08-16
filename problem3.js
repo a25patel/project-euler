82d5bdc70d4d2f5cbe029857b2d3a7476acd63e7
a25patel <aanaletap2510@gmail.com>
2015-08-16 15:43:23 -0600

    working on problem 3

--- problem3.js
+++ problem3.js
@@ -13,10 +13,21 @@ function getPrimes(max) {
     console.log(primes);
 }
 
-getPrimes(50000000);
+// getPrimes(50000000);
+getPrimes(50);
 
 // 2. find the prime factors of any number
+var factors = [];
 function getFactors(number){
-  
+  for (var i = 0; i < primes.length; i++) {
+    if(number % primes[i] === 0)
+    var remainder = number / primes[i]
+    factors.push(primes[i])
+    for (var j = 0; j < primes.length; j++) {
+      if(remainder % primes[i] === 0){
+
+      }
+    }
+  }
 }
 // 3. figure out which of those factors is the largest
