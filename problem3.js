0d01a576c6a20c6c35940caab536e15c6cf0e9d8
a25patel <aanaletap2510@gmail.com>
2015-08-16 21:08:32 -0600

    three solved

--- problem3.js
+++ problem3.js
@@ -1,33 +1,47 @@
-// 1. find prime factors upto a certain number
-function getPrimes(max) {
-    var sieve = [];
-    var primes = [];
-    for (var i = 2; i <= max; i++) {
-        if (!sieve[i]) {
-            primes.push(i);
-            for (var j = i << 1; j <= max; j += i) {
-                sieve[j] = true;
-            }
-        }
-    }
-    console.log(primes);
-}
 
-// getPrimes(50000000);
-getPrimes(50);
-
-// 2. find the prime factors of any number
+// 1. find the prime factors of any number
 var factors = [];
-function getFactors(number){
-  for (var i = 0; i < primes.length; i++) {
-    if(number % primes[i] === 0)
-    var remainder = number / primes[i]
-    factors.push(primes[i])
-    for (var j = 0; j < primes.length; j++) {
-      if(remainder % primes[i] === 0){
-
-      }
+var getPrimeFactors = function(num) {
+    num = Math.floor(num);
+    var root;
+    var doLoop = 1 < num;
+    while( doLoop ){
+        root = Math.sqrt(num);
+        var x = 2;
+        if (num % x) {
+            x = 3;
+            while ((num % x) && ((x += 2) < root));
+        }
+        x = (x > root) ? num : x;
+        factors.push(x);
+        doLoop = ( x != num );
+        num /= x;
     }
-  }
+    console.log(factors);
+    console.log(factors[factors.length - 1]);
 }
-// 3. figure out which of those factors is the largest
+
+getPrimeFactors(600851475143);
+
+
+// 2. figure out which of those factors is the largest
+
+//answer: 71,839,1471,6857
+
+
+// function primeFactorization(num){
+//   var root = Math.sqrt(num),
+//   result = arguments[1] || [],  //get unnamed paremeter from recursive calls
+//   x = 2;
+//
+//   if(num % x){//if not divisible by 2
+//    x = 3;//assign first odd
+//    while((num % x) && ((x = x + 2) < root)){}//iterate odds
+//   }
+//   //if no factor found then num is prime
+//   x = (x <= root) ? x : num;
+//   result.push(x);//push latest prime factor
+//
+//   //if num isn't prime factor make recursive call
+//   return (x === num) ? result : primeFactorization(num/x, result) ;
+// }
